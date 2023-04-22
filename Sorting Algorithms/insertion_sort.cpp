60c8ac25d6850f1d6efc710d854f6127ca4024b1
Ercan Tomaç <ercan2243ercan@gmail.com>
2023-04-22 21:50:06 +0200

    insertion sort

---
+++ Sorting Algorithms/insertion_sort.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int *insertionSort(int array[], int length)
+{
+    for (int i = 0; i < length - 1; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            if (array[i + 1] < array[j])
+            {
+                // SHIFTING ELEMENTS FORWARD BY 1 INDEX
+                int temp = array[i + 1];
+                for (int k = i + 1; k > j; k--)
+                {
+                    array[k] = array[k - 1];
+                }
+
+                // INSERTING THE VALUE INTO WHERE IT NEEDS TO BE
+                array[j] = temp;
+            }
+        }
+    }
+
+    return array;
+}
+
+int main()
+{
+    int array[10] = {23, 80, 756, 2, 99, 1083, 564, 81, 58, 326};
+    int *result = insertionSort(array, 10);
+    for (int i = 0; i < 10; i++)
+    {
+        cout << *(result + i) << endl;
+    }
+
+    return 0;
+}
